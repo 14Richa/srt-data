480c547d6182eaf90d39d3aefa152ede9a1da246
Richa Sharma <onlinericha19@gmail.com>
2019-11-29 23:32:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AwesomeSlider from 'react-awesome-slider';
+import AwesomeSliderStyles from 'react-awesome-slider/src/styled/open-animation/open-animation.scss';
+
+
+const slider = (
+  <AwesomeSlider cssModule={AwesomeSliderStyles}>
+    <div style = {{ backgroundColor: "white" }}>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+  </AwesomeSlider>
+);
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    { slider}
+      
     </div>
+    
   );
 }
 
